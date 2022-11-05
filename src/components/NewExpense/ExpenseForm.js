23308c134c48b87d15af9a3ac3232c11f847e6ea
dhruv481 <dguptadg01@gmail.com>
2022-11-05 23:45:26 +0530

    form data handling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,8 +38,20 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -47,21 +59,11 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            onClick={amountChangeHandler}
-          />
+          <input type="number" min="0.01" step="0.01" onClick={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2025-12-31"
-            onClick={dateChangeHandler}
-          />
+          <input type="date" min="2019-01-01" max="2022-12-31" onClick={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
